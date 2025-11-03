abf343e7388231e7fada2653e9f38da16dd975bc
mcdonaldmakhetho584-ai <mcdonaldmakhetho584@gmail.com>
2025-11-03 14:52:58 +0200

    hi

---
+++ Functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const greeting = greet("Mc Makhetho");
+const sum = add(10, 9);
+
+console.log(greeting);
+console.log(sum);
